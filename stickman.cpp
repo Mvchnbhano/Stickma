1c66af8c4853682a129ee17fb65960e71933abbd
Mvchnbhano <147319105+Mvchnbhano@users.noreply.github.com>
2023-10-08 15:39:40 +0300

    Add files via upload

---
+++ stickman.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "      (°°)     (°°) " << endl;
+    // prints the heads of the stickman and woman
+    cout << "      _||_     _||_              ________" << endl;
+    // prints there upper bodies
+    cout << "     /|  |\\   /|  |\\.           / |    | \\" << endl;
+    // prints their hands and the top of their vehicle
+    cout << "   _/ |__| \\ / |__| \\_       __/  |    |  \\___" << endl;
+    // prints their waist alongside the mid part of their car
+    cout << "      //\\\\  V  /  \\         |                 |" << endl;
+    // prints the identifier if their gender
+    cout << "      |  |    /____\\        |                 |" << endl;
+    cout << "     _|  |_    _||_         ---O------------O--" << endl;
+    // prints their feet alongside the vehicle wheels
+    cout << "-----------------------------------------------------------" << endl;
+    // prints the flat ground on which both the man and woman together with their car are standing
+    return 0;
+}
